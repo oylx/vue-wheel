ebf5afc16fab0c57efc444ea17db48c6bf0d2607
oylx3 <1016655954@Oylxsjh>
2019-03-19 16:57:14 +0800

    icon组件化

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <svg class="icon" aria-hidden="true">
-            <use :xlink:href="`#icon-${icon}`"></use>
-        </svg>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <g-icon v-if="icon" :name="icon"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -10,54 +8,33 @@
 </template>
 <script>
     export default {
-        props:{
-            'icon': {},
-            'icon-position':{
-                type:String,
-                default:'left',
-                validator: function (val) {
-                    return val === 'left' || val === 'right'
+        // props: ['icon', 'iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator (value) {
+                    return value === 'left' || value === 'right'
                 }
             }
         }
     }
 </script>
 <style lang="scss">
-    .g-button{
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding:0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
+    .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+        border-radius: var(--border-radius); border: 1px solid var(--border-color);
         background: var(--button-bg);
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
+        display: inline-flex; justify-content: center; align-items: center;
         vertical-align: middle;
-        &:hover{
-            border-color: var(--border-color-hover);
-        }
-        &:active{
-            background-color: var(--button-active-bg);
-        }
-        &:focus{
-            outline: none;
-        }
-        > .icon{
-            order:1;
-            margin-right: .1em;
-        }
-        >.content{
-            order:2
-        }
-        &.icon-right{
-            > .icon{
-                order:2;
-                margin-left: .1em;
-            }
-            >.content{
-                order:1
-            }
+        &:hover { border-color: var(--border-color-hover); }
+        &:active { background-color: var(--button-active-bg); }
+        &:focus { outline: none; }
+        > .content { order: 2; }
+        > .icon { order: 1; margin-right: .1em; }
+        &.icon-right {
+            > .content { order: 1; }
+            > .icon { order: 2; margin-right: 0; margin-left: .1em;}
         }
     }
 </style>
