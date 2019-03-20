1155383bc45486e49c051c439eacfaa190250113
oylx3 <1016655954@Oylxsjh>
2019-03-20 11:21:04 +0800

    chai，单元测试，window.getComputedStyle

--- src/app.js
+++ src/app.js
@@ -13,3 +13,57 @@ new Vue({
         loading1: false
     }
 })
+
+/**
+ * 测试icon
+ */
+import chai from 'chai'
+const expect = chai.expect;
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'set'
+        }
+    })
+    button.$mount('#a');
+    let useElement = button.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-set')
+}
+
+/**
+ * 测试loading
+ */
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'set',
+            loading:true
+        }
+    })
+    button.$mount('#b');
+    let useElement = button.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+
+}
+/**
+ * 测试icon order
+ */
+{
+    const Constructor = Vue.extend(Button);
+    const div = document.createElement('div');
+    document.body.appendChild(div)
+    const button = new Constructor({
+        propsData:{
+            icon:'set',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+}
