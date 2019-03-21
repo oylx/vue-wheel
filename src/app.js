b425e0aa73962916d2a39406b1a97e0e0a257f11
oylx3 <1016655954@Oylxsjh>
2019-03-21 14:46:06 +0800

    局部注册，单元测试，持续集成（测试、发布），加入.travis.yml

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import  Icon from './icon'
 import  ButtonGroup from './button-group'
 
 Vue.component('g-button',Button)
-Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
@@ -14,82 +13,82 @@ new Vue({
     }
 })
 
-/**
- * 测试icon
- */
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect;
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon:'set'
-        }
-    })
-    vm.$mount('#a');
-    let useElement = vm.$el.querySelector('use');
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-set')
-}
-
-/**
- * 测试loading
- */
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon:'set',
-            loading:true
-        }
-    })
-    vm.$mount('#b');
-    let useElement = vm.$el.querySelector('use');
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-loading')
-
-}
-/**
- * 测试icon order
- */
-{
-    const Constructor = Vue.extend(Button);
-    const div = document.createElement('div');
-    document.body.appendChild(div)
-    const vm = new Constructor({
-        propsData:{
-            icon:'set',
-            iconPosition:'right'
-        }
-    })
-    vm.$mount(div);
-    let svg = vm.$el.querySelector('svg');
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-}
-/**
- * 测试函数点击
- */
-{
-    //mock
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData:{
-            icon:'set'
-        }
-    })
-
-
-
-    vm.$mount()
-
-    let spy = chai.spy(function(){});
-
-    vm.$on('click',spy);
-
-    let button = vm.$el;
-    button.click();
-    expect(spy).to.have.been.called()
-}
+// /**
+//  * 测试icon
+//  */
+// import chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+// const expect = chai.expect;
+// {
+//     const Constructor = Vue.extend(Button);
+//     const vm = new Constructor({
+//         propsData:{
+//             icon:'set'
+//         }
+//     })
+//     vm.$mount('#a');
+//     let useElement = vm.$el.querySelector('use');
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#icon-set')
+// }
+//
+// /**
+//  * 测试loading
+//  */
+// {
+//     const Constructor = Vue.extend(Button);
+//     const vm = new Constructor({
+//         propsData:{
+//             icon:'set',
+//             loading:true
+//         }
+//     })
+//     vm.$mount('#b');
+//     let useElement = vm.$el.querySelector('use');
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#icon-loading')
+//
+// }
+// /**
+//  * 测试icon order
+//  */
+// {
+//     const Constructor = Vue.extend(Button);
+//     const div = document.createElement('div');
+//     document.body.appendChild(div)
+//     const vm = new Constructor({
+//         propsData:{
+//             icon:'set',
+//             iconPosition:'right'
+//         }
+//     })
+//     vm.$mount(div);
+//     let svg = vm.$el.querySelector('svg');
+//     let {order} = window.getComputedStyle(svg)
+//     expect(order).to.eq('2')
+// }
+// /**
+//  * 测试函数点击
+//  */
+// {
+//     //mock
+//     const Constructor = Vue.extend(Button);
+//     const vm = new Constructor({
+//         propsData:{
+//             icon:'set'
+//         }
+//     })
+//
+//
+//
+//     vm.$mount()
+//
+//     let spy = chai.spy(function(){});
+//
+//     vm.$on('click',spy);
+//
+//     let button = vm.$el;
+//     button.click();
+//     expect(spy).to.have.been.called()
+// }
