f17e573bdcf755f07379e2593b0f355e4aef8c91
oylx <oylx2008@163.com>
2019-03-28 11:23:29 +0800

    调试input测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -1,61 +1,61 @@
-// const expect = chai.expect;
-// import Vue from 'vue'
-// import Input from './../src/Input'
-//
-// Vue.config.productionTip = false
-// Vue.config.devtools = false
-//
-// describe('Input', () => {
-//     it('存在.', () => {
-//         expect(Input).to.exist
-//     })
-// })
-// describe('Props',()=>{
-//     const Constructor = Vue.extend(Input);
-//     let vm;
-//     afterEach(()=>{
-//         vm.$destroy();
-//     })
-//     it('接收value.', () => {
-//         vm = new Constructor({
-//             propsData: {
-//                 value: '1234'
-//             }
-//         }).$mount()
-//         const inputElement = vm.$el.querySelector('input')
-//         expect(inputElement.value).to.equal('1234')
-//         vm.$destroy()
-//     })
-//     it('接收disabled.', () => {
-//         vm = new Constructor({
-//             propsData: {
-//                 disabled: true
-//             }
-//         }).$mount()
-//         const inputElement = vm.$el.querySelector('input')
-//         expect(inputElement.disabled).to.equal(true)
-//     })
-//     it('接收readonly.', () => {
-//         vm = new Constructor({
-//             propsData: {
-//                 readonly: true
-//             }
-//         }).$mount()
-//         const inputElement = vm.$el.querySelector('input')
-//         expect(inputElement.readOnly).to.equal(true)
-//     })
-//     it('接收error.', () => {
-//         vm = new Constructor({
-//             propsData: {
-//                 error: 'wrong'
-//             }
-//         }).$mount();
-//         const useElement = vm.$el.querySelector('use');
-//         expect(useElement.getAttribute('xlink:href')).to.equal('#icon-error')
-//         const errorMessage = vm.$el.querySelector('.errorMessage')
-//         expect(errorMessage.innerText).to.equal('wrong')
-//     })
-// })
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from './../src/Input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+    it('存在.', () => {
+        expect(Input).to.exist
+    })
+})
+describe('Props',()=>{
+    const Constructor = Vue.extend(Input);
+    let vm;
+    afterEach(()=>{
+        vm.$destroy();
+    })
+    it('接收value.', () => {
+        vm = new Constructor({
+            propsData: {
+                value: '1234'
+            }
+        }).$mount()
+        const inputElement = vm.$el.querySelector('input')
+        expect(inputElement.value).to.equal('1234')
+        vm.$destroy()
+    })
+    it('接收disabled.', () => {
+        vm = new Constructor({
+            propsData: {
+                disabled: true
+            }
+        }).$mount()
+        const inputElement = vm.$el.querySelector('input')
+        expect(inputElement.disabled).to.equal(true)
+    })
+    it('接收readonly.', () => {
+        vm = new Constructor({
+            propsData: {
+                readonly: true
+            }
+        }).$mount()
+        const inputElement = vm.$el.querySelector('input')
+        expect(inputElement.readOnly).to.equal(true)
+    })
+    it('接收error.', () => {
+        vm = new Constructor({
+            propsData: {
+                error: 'wrong'
+            }
+        }).$mount();
+        const useElement = vm.$el.querySelector('use');
+        expect(useElement.getAttribute('xlink:href')).to.equal('#icon-error')
+        const errorMessage = vm.$el.querySelector('.errorMessage')
+        expect(errorMessage.innerText).to.equal('wrong')
+    })
+})
 // describe('事件',()=>{
 //     const Constructor = Vue.extend(Input);
 //     let vm;
